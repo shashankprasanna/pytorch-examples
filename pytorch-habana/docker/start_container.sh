50fa4e6380ccf6ec3fd651b4c79f1cf9db1fe4de
shashankprasanna <shashank4@gmail.com>
2023-02-09 00:21:52 +0000

    updates

---
+++ pytorch-habana/docker/start_container.sh
@@ -0,0 +1,3 @@
+docker build -t pytorch-habana:latest -f Dockerfile .
+docker rmi $(docker images -f dangling=true -q)
+docker run -it --rm --network=host -v $PWD/:/pytorch-habana --workdir /pytorch-habana pytorch-habana:latest
