1a72512ccc8581b3f3f549d86a81e2c7e5b84950
shashankprasanna <shashank4@gmail.com>
2023-03-19 21:00:35 +0000

    test

--- pytorch-torchcompile/start_container.sh
+++
@@ -1,4 +0,0 @@
-docker rm $(docker ps -a -q)
-docker build -t pytorch2:latest .
-docker rmi $(docker images -f dangling=true -q)
-docker run -it --rm --network=host --gpus all -v $PWD:/pytorch-examples --workdir /pytorch-examples pytorch2:latest
